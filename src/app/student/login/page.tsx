d8f91081f79ce39db7c596c91719239e56e308ef
Ajo Alex <67946056+devpenzil@users.noreply.github.com>
2024-01-14 21:41:32 +0530

    input

---
+++ src/app/student/login/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import InputField from '@/app/components/InputField'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div className='h-screen w-full bg-[#E1E1FF] flex flex-col justify-center items-center' >
+      <InputField/>
+    </div>
+  )
+}
+
+export default Page
