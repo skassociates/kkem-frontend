6bd79a799a972702cb9e73a62d25c1f276395012
OneWhoShouldNotBeNamed <maheshsk019@gmail.com>
2024-01-29 20:58:04 +0530

    career admin updated

--- src/app/admin/instructions/page.tsx
+++ src/app/admin/instructions/page.tsx
@@ -12,43 +12,33 @@ const Page = () => {
           Instructions
         </div>
         <div className="p-9">
-          <div className="text-3xl">
-            Connect Career to Campus 2023 - <br />
-            Career Ambassodors Leaderboard Form
-          </div>
+          <div className="text-3xl">Career Ambassadors</div>
           <div className="mt-8 text-lg">
             As part of the Connect Career to Campus (CCC) Campaign, KKEM is
             proud to introduce the{" "}
             <span className="font-bold">CCC Leaderboard</span> to help you
-            analyse your employability skills. Through a series of questions and
-            answers, we would like you to embark on this journey and become a
-            top scorer on the CCC Leaderboard! Complete all the required
-            activities, earn maximum points, and stand a chance to be the CCC
-            Leaderboard Topper! <br /> <br />
+            analyse and evaluate your experience as a Career Ambassador in your
+            institution. Through a series of questions and answers, we would
+            like you to embark on this journey and become a top scorer on the
+            CCC Leaderboard! Complete all the required activities, earn maximum
+            points, and stand a chance to be a top performing Career Ambassador!
             Kindly take a few moments to read the instructions below: <br />{" "}
+            <br />
+            <div>
+              Kindly take a few moments to read the instructions below:
+            </div>{" "}
             <br />
             <ul className="list-decimal list-outside ml-5">
               <li>
-                All registered students are requested to answer the following
-                questions and be a part of the CCC Leaderboard. Ensure that you
-                are using the DWMS registered Email Address and DWMS ID to login
-                and submit your responses.{" "}
-              </li>
-              <li>
-                A few questions would require you to submit a proof that
-                supports your response. Please keep it ready to ease the
-                process.
-              </li>
-              <li>
-                Please follow this naming convention for the supporting
-                documents - DWMS ID_Your Name_Proof for (Name of Section).{" "}
-                <br />
-                For instance, DW101010_Ann Michael_Proof for WRP
+                All registered career ambassadors are requested to answer the
+                following questions and be a part of the CCC Leaderboard. Ensure
+                that you are using the DWMS registered Email Address and DWMS ID
+                to login and submit your responses.
               </li>
               <li>
                 Kindly ensure that any Personal Identifiable Information (PII)
                 in the document is masked before uploading. KKEM is in no way
-                responsible for the loss of PII from these documents.{" "}
+                responsible for the loss of PII from these documents.
               </li>
               <li>
                 Please enter accurate details for all the questions, as all the
