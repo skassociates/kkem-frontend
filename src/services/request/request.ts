55fb0a1431e39a2457bc2e7a7d05231662b8c0f0
Mahesh <mahesh.sk@neoito.com>
2024-05-27 14:51:01 +0530

    change to http

--- src/services/request/request.ts
+++ src/services/request/request.ts
@@ -10,7 +10,7 @@ try {
 export const axiosInstance = axios.create({
   // baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   // baseURL: "http://localhost:3001/",
-  baseURL: "http://52.66.197.135:3001",
+  baseURL: "https://52.66.197.135:3001",
   headers: {
     // 'Content-Type': "application/json",
     Authorization: token,
