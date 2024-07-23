3f08598c619313760d99a802b160d46103103d80
Mahesh <mahesh.sk@neoito.com>
2024-07-23 07:50:40 +0530

    req updated

--- src/services/request/request.ts
+++ src/services/request/request.ts
@@ -8,9 +8,8 @@ try {
 } catch (error) {}
 
 export const axiosInstance = axios.create({
-  // baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-  // baseURL: "http://localhost:3001/",
-  baseURL: "https://52.66.197.135/",
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+
   headers: {
     // 'Content-Type': "application/json",
     Authorization: token,
